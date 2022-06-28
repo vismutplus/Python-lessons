d3639e7fa2f55981e93e53e2530264f8433b913e
vismutplus <mihaprofi@ya.ru>
2022-06-28 18:57:48 +0300

    Функции и переменные

--- study.py
+++ study.py
@@ -9,3 +9,28 @@ elif (age >= 18) and (age < 25):
     print('Вход только с мамой')
 else:
     print('Иди взрослей!')
+
+
+def hello():
+    print('Урок № 3')
+
+
+hello()
+
+x = int(input('Введите 1 число:'))
+y = int(input('Введите 2 число:'))
+
+
+def sum(a, b):
+    return a+b
+
+
+z = (sum(x, y))
+print(z)
+
+
+def f(a):
+    return 4*a-3
+
+
+print(f(5))
