0c6f7adbde7bc5838c1f044a079a3d288e48238f
vismutplus <mihaprofi@ya.ru>
2022-06-28 18:34:55 +0300

    переменные

--- help.py
+++ help.py
@@ -1,3 +1,11 @@
-message = 'Привет мир!'
+message = 'Привет мир! А тебе есть 18?'
 
 print(message)
+
+age = int(input('Сколько тебе лет?'))
+if (age >= 25):
+    print('Добро пожаловать!')
+elif (age >= 18) and (age < 25):
+    print('Вход только с мамой')
+else:
+    print('Иди взрослей!')
